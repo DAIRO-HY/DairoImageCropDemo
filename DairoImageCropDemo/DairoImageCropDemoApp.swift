c39360a7363fdaac367b057057ea6271d585ce17
zhoulq <615741631@qq.com>
2024-03-18 00:26:56 +0900

    第一次提交

---
+++ DairoImageCropDemo/DairoImageCropDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DairoImageCropDemoApp.swift
+//  DairoImageCropDemo
+//
+//  Created by zhoulq on 2024/03/17.
+//
+
+import SwiftUI
+
+@main
+struct DairoImageCropDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
